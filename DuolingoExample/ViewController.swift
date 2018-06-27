fc99d337d2d9747a72f34634c20f22caaafcb209
Gokhan Gultekin <gokhangultekin07@hotmail.com>
2018-06-27 22:01:01 +0300

    First commit

---
+++ DuolingoExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DuolingoExample
+//
+//  Created by Gokhan Gultekin on 27.06.2018.
+//  Copyright © 2018 Gokhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
