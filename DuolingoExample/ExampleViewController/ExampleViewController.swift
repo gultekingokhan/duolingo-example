984fa4c03266de23a4b581d492c7e065caa98332
Gokhan Gultekin <gokhangultekin07@hotmail.com>
2018-06-27 22:19:04 +0300

    Project initiation completed. Ready to code.

---
+++ DuolingoExample/ExampleViewController/ExampleViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleViewController.swift
+//  DuolingoExample
+//
+//  Created by Gokhan Gultekin on 27.06.2018.
+//  Copyright © 2018 Gokhan. All rights reserved.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
